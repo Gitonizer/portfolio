b2e9e164c38a4ec86259d0d8ad511d8e512d17cd
bruno <brunotfatima@gmail.com>
2024-05-21 21:18:29 +0100

    included social links

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,6 +6,9 @@ import {
 } from "@heroicons/react/24/solid";
 import { useState, useContext } from "react";
 import SideBarContext from "../store/sidebar-context";
+import { SocialIcon } from 'react-social-icons/component';
+import 'react-social-icons/linkedin'
+import 'react-social-icons/github';
 
 function SideBar() {
   const [region, setRegion] = useState("#about");
@@ -55,6 +58,11 @@ function SideBar() {
           <div className={classes.text}>Past Work</div>
         </div>
       </a>
+      <div className={classes.socialIcons}>
+        <SocialIcon url="https://github.com/Gitonizer" style={{margin:"auto", width: "33px", height: "33px"}}/>
+        <SocialIcon url="https://www.linkedin.com/in/bruno-t-fatima/" style={{margin:"auto", width: "33px", height: "33px"}}/>
+        <div />
+      </div>
     </div>
   );
 }
